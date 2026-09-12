501f59f93a741acb63cfcb56a46471fcfbb74808
Matvey Yangolenko <Tyuiu.YangolenkoM@outlook.com>
2026-09-12 19:16:15 +0500

    Task1

--- Tyuiu.YangolenkoMS.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.YangolenkoMS.Sprint0.Task1.V0/Program.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.YangolenkoMS.Sprint0.Task1.V0
             Console.WriteLine("Hello, World");
             string name = " Матвей ";
             int age = 17;
-            Console.WriteLine("Здравствуйте меня зовут" +  name  + "мне " +  age  + " лет! ");
+            Console.WriteLine("Здравствуйте меня  зовут" +  name  + "мне " +  age  + " лет! ");
             Console.ReadKey();
         }
     }
