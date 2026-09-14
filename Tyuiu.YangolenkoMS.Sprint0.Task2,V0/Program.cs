aaee44f8fee11bfdd6237a04a06a021cb4b3b1b2
Matvey Yangolenko <Tyuiu.YangolenkoM@outlook.com>
2026-09-14 13:33:12 +0500

    Task2

--- Tyuiu.YangolenkoMS.Sprint0.Task2,V0/Program.cs
+++ Tyuiu.YangolenkoMS.Sprint0.Task2,V0/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Tyuiu.YangolenkoMS.Sprint0.Task2.V0.Lib;
 
 namespace Tyuiu.YangolenkoMS.Sprint0.Task2_V0
